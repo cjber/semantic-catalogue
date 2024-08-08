4281f92b96d6941c1040ccdffbafcf202d21e878
cjber <cjberragan@gmail.com>
2024-08-08 13:56:05 +0100

    begin adding tests

--- src/datastore/__init__.py
+++ src/datastore/__init__.py
@@ -1,6 +1,7 @@
 from dagster import Definitions, load_assets_from_modules
 
 from src.datastore.assets import adr, cdrc, datastore, ukds
+from src.datastore.assets.test_assets.adr import adr_datasets_id_no_nulls
 from src.datastore.jobs import adr_job, cdrc_job, ukds_job
 from src.datastore.resources import openai_resource
 from src.datastore.schedules import adr_schedule, cdrc_schedule, ukds_schedule
@@ -12,6 +13,7 @@ datastore_assets = load_assets_from_modules(modules=[datastore], group_name="dat
 
 defs = Definitions(
     assets=[*ukds_assets, *adr_assets, *cdrc_assets, *datastore_assets],
+    # asset_checks=[adr_datasets_id_no_nulls],
     jobs=[adr_job, ukds_job, cdrc_job],
     schedules=[adr_schedule, ukds_schedule, cdrc_schedule],
     resources={"openai": openai_resource},
