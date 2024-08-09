8aa6d516dedb12eef151392dccd2789bfb7acc26
cjber <cjberragan@gmail.com>
2024-08-09 09:58:56 +0100

    refactor endpoints

--- src/search_api/api.py
+++ src/search_api/api.py
@@ -1,54 +1,47 @@
+from typing import Union
 from uuid import UUID, uuid4
 
 from fastapi import FastAPI
 from langchain_core.documents import Document
 
-from src.model.model import generation_graph, search_graph
+from src.model.model import generate, search
 
 app = FastAPI()
 document_store = {}
 query_mapping = {}
 
 
-search_app = search_graph()
-gen_app = generation_graph()
-
-
 @app.get("/")
-def index():
+def index() -> dict[str, str]:
     return {"message": "Make a post request to /query."}
 
 
 @app.post("/query")
-async def query(q: str) -> dict:
-    thread_id = uuid4()
-    out = search_app.invoke(
-        {"query": q}, config={"configurable": {"thread_id": thread_id}}
-    )
+async def query(q: str) -> dict[str, Union[UUID, str, list[dict]]]:
+    results_id = uuid4()
+    out = search(query=q, thread_id=results_id)
+
     docs_dict = [d.dict() for d in out["documents"]]
-    document_store[thread_id] = docs_dict
-    query_mapping[thread_id] = q
-    return {"thread_id": thread_id, "query": q, "documents": docs_dict}
+    document_store[results_id] = docs_dict
+    query_mapping[results_id] = q
+    return {"results_id": results_id, "query": q, "documents": docs_dict}
 
 
-@app.get("/explain/{thread_id}")
-async def explain(thread_id: UUID, docid: int) -> dict:
-    doc_dict = document_store[thread_id][docid]
+@app.get("/explain/{results_id}")
+async def explain(results_id: UUID, docid: int) -> dict:
+    doc_dict = document_store[results_id][docid]
     document = Document(
         page_content=doc_dict["page_content"],
         metadata=doc_dict["metadata"],
     )
-    query = query_mapping[thread_id]
-    generation_state = gen_app.invoke(
-        {"query": query, "document": document},
-        config={"configurable": {"thread_id": thread_id}},
-    )
-    generation = generation_state["generation"]
+    query = query_mapping[results_id]
+    out = generate(query=query, document=document, thread_id=results_id)
+    generation = out["generation"]
 
     return {
         "generation": generation,
         "metadata": {
-            "thread_id": thread_id,
+            "results_id": results_id,
             "query": query,
             "related_dataset": doc_dict,
         },
