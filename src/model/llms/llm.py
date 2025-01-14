d57ea0f5060b7ccadf6907dfbe3b7656e0a5dee9
cjber <cjberragan@gmail.com>
2025-01-14 13:46:31 +0000

    refactor: change 'src' dir to 'semantic_catalogue'

--- src/model/llms/llm.py
+++
@@ -1,5 +0,0 @@
-from langchain_openai import ChatOpenAI
-
-from src.common.settings import cfg
-
-LLM = ChatOpenAI(model=cfg.model.llm, temperature=0)
--- src/model/logging.py
+++
@@ -1,3 +0,0 @@
-from loguru import logger
-
-logger.add("logs/model.log", rotation="1 MB", retention="10 days", level="INFO")
